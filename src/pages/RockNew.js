db8cbca82df0cdd165326d2cf954a27c82117cfe
charper95 <charper1995@gmail.com>
2021-08-23 16:46:05 -0700

    adds some styling to pages

--- src/pages/RockNew.js
+++ src/pages/RockNew.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 class RockNew extends Component {
   render() {
     return (
-      <>
+      <div className="page container">
         <h3>I am the Rock New page</h3>
-      </>
+      </div>
     )
   }
 }
--- src/pages/RockShow.js
+++ src/pages/RockShow.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 class RockShow extends Component {
   render() {
     return (
-      <>
+      <div className="page container">
         <h3>I am the Rock Show page</h3>
-      </>
+      </div>
     )
   }
 }
