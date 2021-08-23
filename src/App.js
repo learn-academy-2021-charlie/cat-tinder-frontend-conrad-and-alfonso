457ba62aa84a23d18cd14050e2feb1c1ae1bb71b
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-08-23 16:11:22 -0700

    moved styling import to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import "./App.css";
+
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
