b94110602774259c93d385158d308db407dc768c
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-08-25 10:32:39 -0700

    added form to RockNew

--- src/pages/RockNew.js
+++ src/pages/RockNew.js
@@ -1,10 +1,81 @@
 import React, { Component } from 'react'
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 class RockNew extends Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      form: {
+        name:"",
+        color:"",
+        brilliance:'NaN',
+        hardness:'NaN'
+      }
+    }
+  }
+
+  handleChange = (e) =>{
+    let {form} = this.state
+    form[e.target.name] = e.target.value
+    this.setState({form: form})
+  }
+
+
+  handleSubmit = () => {
+    console.log(this.state.form)
+  }
+
+
   render() {
     return (
       <div className="page container">
         <h3>I am the Rock New page</h3>
+        <Form>
+        <br/>
+          <FormGroup>
+            <Label for="name">Name</Label>
+            <Input 
+              type="text" 
+              name="name" 
+              onChange={this.handleChange} 
+              value={this.state.form.name}
+            />
+          </FormGroup>
+          <br/>
+          <FormGroup>
+            <Label for="color">Color</Label>
+            <Input 
+              type="text" 
+              name="color" 
+              onChange={this.handleChange} 
+              value={this.state.form.color}
+            />
+          </FormGroup>
+          <br/>
+          <FormGroup>
+            <Label for="brilliance">Brilliance (between 1 and 10)</Label>
+            <Input 
+              type="number" 
+              name="brilliance" 
+              min="0" max="10" 
+              onChange={this.handleChange} 
+              value={this.state.form.brilliance}
+            />
+          </FormGroup>
+          <br/>
+          <FormGroup>
+            <Label for="hardness">Hardness (between 1 and 10)</Label>
+            <Input 
+              type="number" 
+              name="hardness" 
+              min="0" max="10" 
+              onChange={this.handleChange} 
+              value={this.state.form.hardness}
+            />
+          </FormGroup>
+          <br/>
+          <Button name="submit" color="success" onClick={this.handleSubmit}>Create new Rock</Button>
+        </Form>
       </div>
     )
   }
