c69e2277fda7e57373af0b921912ce228b9bea53
charper95 <charper1995@gmail.com>
2021-08-25 16:48:17 -0700

    adds cards

--- src/pages/RockIndex.js
+++ src/pages/RockIndex.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import {
+  Card, Button, CardTitle, CardText, CardGroup,
+  CardSubtitle, CardBody
+} from 'reactstrap';
 
 class RockIndex extends Component {
   render() {
     return (
       <div className="page container">
         <h3>I am the Rock Index page</h3>
-        {this.props.rocks && this.props.rocks.map(rock => {
-          return (
-          <p key={rock.id}>
-            <Link to={`/rockshow/${rock.id}`}>
-            {rock.name}</Link>
-          </p>
-          )
-        })}
+        <CardGroup>
+          {this.props.rocks && this.props.rocks.map(rock => {
+            return (
+              <Card key={rock.id}>
+                <CardBody>
+                  <CardTitle tag="h5">Card title</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+                  <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+                  <Button>
+                    <Link to={`/rockshow/${rock.id}`}>
+                      {rock.name}
+                    </Link>
+                  </Button>
+                </CardBody>
+              </Card>
+            )
+          })}
+        </CardGroup>
+
+
+
       </div>
     )
   }
