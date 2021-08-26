525e23771a73adbc302e117ca1f9c9bed5393eb1
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-08-26 16:27:48 -0700

    styled show page

--- src/pages/RockShow.js
+++ src/pages/RockShow.js
@@ -7,18 +7,18 @@ class RockShow extends Component {
    const {rock} = this.props
     return (
       <div className="page container">
-        <h3>I am the Rock Show page</h3>
-        <p><strong>Name:</strong> {rock && rock.name}</p>
-        <p><strong>Color:</strong> {rock && rock.color}</p>
-        <p className="brilliance"><strong>Brilliance:</strong> {rock && rock.brilliance}</p>
-        <Progress value={rock && rock.brilliance * 10}>{rock && rock.brilliance}/10</Progress>
-        <p className="hardness" ><strong>Hardness:</strong> {rock && rock.hardness}</p>
-        <Progress value={rock && rock.hardness * 10} color="success">{rock && rock.hardness}/10</Progress>
-        <br/>
-        <Link to={`/rockedit/${rock && rock.id}`}><Button color="success">Edit Rock</Button></Link>
-        <br/>
-        <br/>
-        <Link to="/rockindex"><Button color="primary">Back to Rocks</Button></Link>
+        <div className="box">
+          <h3>I am the Rock Show page</h3>
+          <p><strong>Name:</strong> {rock && rock.name}</p>
+          <p><strong>Color:</strong> {rock && rock.color}</p>
+          <p className="brilliance"><strong>Brilliance:</strong> {rock && rock.brilliance}</p>
+          <Progress value={rock && rock.brilliance * 10}>{rock && rock.brilliance}/10</Progress>
+          <p className="hardness" ><strong>Hardness:</strong> {rock && rock.hardness}</p>
+          <Progress value={rock && rock.hardness * 10} color="success">{rock && rock.hardness}/10</Progress>
+          <br/>
+          <Link to={`/rockedit/${rock && rock.id}`}><Button color="success">Edit Rock</Button></Link>
+          <Link to="/rockindex"><Button color="primary">Back to Rocks</Button></Link>
+        </div>
       </div>
     )
   }
