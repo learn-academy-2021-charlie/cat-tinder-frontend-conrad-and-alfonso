aacfa03484b5c4d9620b33314725307c796a2279
charper95 <charper1995@gmail.com>
2021-08-26 12:00:04 -0700

    adds card styling for frontend

--- src/pages/RockIndex.js
+++ src/pages/RockIndex.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 import {
-  Card, Button, CardTitle, CardText, CardGroup,
-  CardSubtitle, CardBody
+  Card, Button, CardTitle, CardGroup, CardBody
 } from 'reactstrap';
+import { GiRockGolem } from 'react-icons/gi'
 
 class RockIndex extends Component {
   render() {
     return (
       <div className="page container">
-        <h3>I am the Rock Index page</h3>
+        <h3 className="text-center">I am the Rock Index page</h3>
         <CardGroup>
           {this.props.rocks && this.props.rocks.map(rock => {
             return (
               <Card key={rock.id}>
                 <CardBody>
-                  <CardTitle tag="h5">Card title</CardTitle>
-                  <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                  <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                  <Button>
-                    <Link to={`/rockshow/${rock.id}`}>
-                      {rock.name}
+                  <CardTitle tag="h5">{rock.name}</CardTitle>
+                  <h1 className="mb-3"><GiRockGolem /></h1>
+                  <Button color="success">
+                    <Link className="index-button-link" to={`/rockshow/${rock.id}`}>
+                      Go to Rock
                     </Link>
                   </Button>
                 </CardBody>
